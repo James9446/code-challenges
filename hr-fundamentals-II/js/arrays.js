e60fea3cb96210422107847509d611fb9bb93890
James Reynolds <jameshreynolds@gmail.com>
2017-11-18 13:51:03 -0800

    Added folder created at fundamentals II class.

---
+++ hr-fundamentals-II/js/arrays.js
@@ -0,0 +1,14 @@
+// Write a function rest that returns all the elements in the array except for the first one. HINT: Read about the slice method on MDN and/or experiment with slice at the console like so:
+
+// var numbers = [3, 2, 7, 5];
+
+
+
+// console.log(numbers.slice(0),
+// numbers.slice(1),
+// numbers.slice(2),
+// numbers.slice(0, 2));
+
+// Write a function butlast that returns all of the elements in the array except for the last one 
+// (you may want to use slice for this one as well).
+
